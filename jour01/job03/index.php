e75fb9c09479c6a91d2abaa548e4bf20b27c1bca
Astabruti69 <sangeet.acharya@laplateforme.io>
2025-07-09 09:26:09 +0200

    runtrack2 job 3 fini avec des embellisement

--- jour01/job03/index.php
+++ jour01/job03/index.php
@@ -13,29 +13,28 @@ $type = True;
 $nom = "Acharya";
 $valeur = 0.1;
 
-
 $val = 3 ;
 ?>
 <table class="tab1" border="1" cellpadding="20" cellspacing="20">
-    <tr>
+    <tr class="tr1">
       <th>Type</th>
       <th>Nom</th>
       <th>Valeur</th>
     </tr>
   
-<tr>
+    <tr class="tr2">
       <td><?php echo gettype ($valeur) ?></td>
-      <td>valeur</td>
+      <td>1rst</td>
       <td><?php echo $valeur ?></td>
     </tr>
-    <tr>
+    <tr class="tr3">
       <td><?php echo gettype ($type) ?></td>
-      <td>type</td>
+      <td>2nd</td>
       <td><?php echo $type ?></td>
     </tr>
-    <tr>
+    <tr class="tr4">
       <td><?php echo gettype ($nom) ?></td>
-      <td>nom</td>
+      <td>3th</td>
       <td><?php echo $nom ?></td>
     </tr>
     </table>
