b4ea50a998b776f1f39fcc255a33c83be230d238
Astabruti69 <sangeet.acharya@laplateforme.io>
2025-07-28 16:27:00 +0200

    revision jour 3 job 2 fini

---
+++ jour06/Job03/Jour03/Job02/index.php
@@ -0,0 +1,14 @@
+<?php
+$str ="Tous ces instants 
+    seront perdus dans le temps 
+    comme les larmes sous la pluie";
+$result ="";
+
+for ($i=0; $i <strlen($str) ; $i+=2) { 
+    $result=$str[$i];
+    echo $result;
+}
+?>
+<!-- $a += 5; // 
+ affecte la valeur 8 à la variable $a correspond à l'instruction 
+ '$a = $a + 5'; -->
