ce8afb3237066379ddd0166d166a1ffc91a8b645
Astabruti69 <sangeet.acharya@laplateforme.io>
2025-09-24 10:49:28 +0200

    Create index.php

---
+++ jour10/Job01/index.php
@@ -0,0 +1,5 @@
+<!-- En utilisant php et mysqli, connectez-vous à la base de données “jour09”. A l’aide d’une
+requête SQL, récupérez l’ensemble des informations de la table etudiants. Affichez le
+résultat de cette requête dans un tableau html. La première ligne de votre tableau html
+(thead) doit contenir le nom des champs. Les suivantes (tbody) doivent contenir les
+données présentes dans votre base de données. -->
